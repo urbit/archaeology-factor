2e2b7b13744d2c027811fd13b29ad435aee07081
Galen Wolfe-Pauly <mailroom@office-for.com>
2015-04-17 18:29:19 -0700

    a mess, for now

---
+++ urb/zod/main/pub/work/src/js/main.js
@@ -0,0 +1,19 @@
+$(function() {
+  console.log('ready')  
+  $('body').on('click',function() {
+    window.urb.send({
+      appl:"work",
+      path:"/",
+      mark:"work-command",
+      data:{
+        'create':{
+          'task':1,
+          'title':'go and!'
+        }
+      }
+    },function(err,res) {
+      console.log('send')
+      console.log(arguments)
+    })
+  })
+})
